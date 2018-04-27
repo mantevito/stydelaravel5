65d2ec1db6da1401a296f9b17f7b30006c10b85a
mantevito <mantevito@hotmail.com>
2018-04-27 10:40:41 +0200

    30 Hecho

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,20 +6,34 @@
 @section('content')
     <h1>Crear usuario</h1>
 
-<form method="POST" action="{{ url('usuarios') }}">
-    {!! csrf_field() !!}
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <h6>Por favor corrige los siguientes errores</h6>
+{{--            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>--}}
+        </div>
 
-    <label for="name">Nombre:</label>
-    <input type="text" name="name" id="name" placeholder="Pedro Perez">
+    @endif
 
-    <label for="email">Email:</label>
-    <input type="email" name="email" id="email" placeholder="pedro@example.com">
+    <form method="POST" action="{{ url('usuarios') }}">
+        {!! csrf_field() !!}
 
-    <label for="password">Contraseña:</label>
-    <input type="password" name="password" id="password"placeholder="Mayor a 6 caracteres">
+        <label for="name">Nombre:</label>
+        <input type="text" name="name" id="name" placeholder="Pedro Perez" value="{{old('name')}}">
+        @if($errors->has('name'))
+            <p>{{ $errors->first('name') }}</p>
+        @endif
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" placeholder="pedro@example.com" value="{{old('email')}}">
 
-    <button type="submit">Crear usuario</button>
-</form>
+        <label for="password">Contraseña:</label>
+        <input type="password" name="password" id="password"placeholder="Mayor a 6 caracteres">
+
+        <button type="submit">Crear usuario</button>
+    </form>
 
     <!--<p><a href="{{ url('/usuarios') }}">Regresar</a></p>-->
     <p><a href="{{ route('users.index') }}">Regresar al listado de usuarios</a></p>
